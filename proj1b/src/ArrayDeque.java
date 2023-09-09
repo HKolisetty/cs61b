235c3064c5a9bb92620006faadf83577e9cebb80
Harshit Kolisetty <harshitk@berkeley.edu>
2023-09-09 01:44:45 -0700

    Finished proj1b

---
+++ proj1b/src/ArrayDeque.java
@@ -0,0 +1,209 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.Math;
+
+public class ArrayDeque<T> implements Deque<T> {
+    private T[] array;
+    private int size, startIndex, endIndex;
+
+    /**
+     * Construct new empty list
+     */
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        size = 0;
+        startIndex = 0;
+        endIndex = 0;
+    }
+
+    /**
+     * Private helper function to geometrically resize array up by a factor of {@code n}
+     * @param n factor to resize array up by
+     */
+    private void resizeUp(int n) {
+        T[] newArray = (T[]) new Object[array.length * n];
+        boolean flag = false;
+        for (int i = 0; i < array.length; i++) {
+            if (i <= endIndex && !flag) {
+                newArray[i] = array[i];
+            }
+            else {
+                newArray[i + array.length] = array[i];
+            }
+            if (i == startIndex) {
+                startIndex = i + array.length;
+                flag = true;
+            }
+        }
+        array = newArray;
+    }
+
+    /**
+     * Add {@code x} of type T to the front of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item of datatype T to add
+     */
+    @Override
+    public void addFirst(T x) {
+        if (size == array.length) {
+            resizeUp(2);
+        }
+        array[startIndex] = x;
+        size += 1;
+        startIndex -= 1;
+        if (startIndex < 0) {
+            startIndex = array.length - 1;
+        }
+        if (size == 1) {
+            endIndex += 1;
+        }
+    }
+
+    /**
+     * Add {@code x} of type T to the back of the deque. Assumes {@code x} is never null.
+     *
+     * @param x item of datatype T to add
+     */
+    @Override
+    public void addLast(T x) {
+        if (size == array.length) {
+            resizeUp(2);
+        }
+        array[endIndex] = x;
+        size += 1;
+        endIndex += 1;
+        if (size == 1) {
+            startIndex = array.length - 1;
+        }
+    }
+
+    /**
+     * Converts the deque into a list.
+     *
+     * @return List<T> list containing deque items in order
+     */
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        int startValue = startIndex + 1;
+        if (startValue == array.length) {
+            startValue = 0;
+        }
+        for (int i = startValue; i < endIndex || i > startIndex; i++) {
+            if (startIndex <= endIndex && i >= endIndex) {
+                break;
+            }
+            returnList.add(array[i]);
+            if (i == array.length - 1) {
+                i = -1;
+            }
+        }
+        return returnList;
+    }
+
+    /**
+     * @return boolean for whether deque is empty.
+     */
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * @return int for number of items in deque.
+     */
+    @Override
+    public int size() {
+        return size;
+    }
+
+    /**
+     * Private helper function to geometrically resize array down by a factor of {@code n}
+     * @param n factor to resize array down by
+     */
+    private void resizeDown(int n) {
+        T[] newArray = (T[]) new Object[(int) (double) (array.length / n)];
+        int change = array.length - newArray.length;
+        for (int i = 0; i < newArray.length; i++) {
+            if (i <= endIndex) {
+                newArray[i] = array[i];
+            }
+            else {
+                newArray[i] = array[i + change];
+            }
+            if (i + change == startIndex) {
+                startIndex = i;
+            }
+        }
+        array = newArray;
+    }
+
+    /**
+     * Remove the first item from the deque
+     *
+     * @return item that was removed or null if deque is empty
+     */
+    @Override
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+        int startValue = startIndex + 1;
+        if (startValue == array.length) {
+            startValue = 0;
+        }
+        T returnItem = array[startValue];
+        array[startValue] = null;
+        startIndex = startValue;
+        if (size > 16 && (double) size /array.length < 0.25) {
+            resizeDown(2);
+        }
+        return returnItem;
+    }
+
+    /**
+     * Remove the last item from the deque
+     *
+     * @return item that was removed or null if deque is empty
+     */
+    @Override
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        int endValue = endIndex - 1;
+        if (endValue < 0) {
+            endValue = array.length - 1;
+        }
+        T returnItem = array[endValue];
+        array[endValue] = null;
+        endIndex = endValue;
+        if (size > 16 && (double) size /array.length < 0.25) {
+            resizeDown(2);
+        }
+        return returnItem;
+    }
+
+    /**
+     * Return the item at the specified index
+     *
+     * @param index of item
+     * @return item specified by index or null if index is out of range
+     */
+    @Override
+    public T get(int index) {
+        if (index > array.length || index < 0) {
+            return null;
+        }
+        return array[index];
+    }
+
+    /**
+     * Error thrown when this unnecessary method is called
+     */
+    @Override
+    public T getRecursive(int index) {
+        throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
+    }
+}
--- proj1b/tests/ArrayDequeTest.java
+++ proj1b/tests/ArrayDequeTest.java
@@ -10,14 +10,143 @@ import static com.google.common.truth.Truth.assertWithMessage;
 
 public class ArrayDequeTest {
 
-    // @Test
-    // @DisplayName("ArrayDeque has no fields besides backing array and primitives")
-    // void noNonTrivialFields() {
-    //     List<Field> badFields = Reflection.getFields(ArrayDeque.class)
-    //             .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
-    //             .toList();
+     @Test
+     @DisplayName("ArrayDeque has no fields besides backing array and primitives")
+     void noNonTrivialFields() {
+         List<Field> badFields = Reflection.getFields(ArrayDeque.class)
+                 .filter(f -> !(f.getType().isPrimitive() || f.getType().equals(Object[].class) || f.isSynthetic()))
+                 .toList();
 
-    //     assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
-    // }
+         assertWithMessage("Found fields that are not array or primitives").that(badFields).isEmpty();
+     }
 
+     @Test
+     void add_first_only() {
+         Deque<Integer> arr1 = new ArrayDeque<>();
+         arr1.addFirst(2);
+         arr1.addFirst(1);
+         arr1.addFirst(0);
+         assertThat(arr1.toList()).containsExactly(0,1,2);
+     }
+    @Test
+    void add_last_only() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addLast(0);
+        arr1.addLast(1);
+        arr1.addLast(2);
+        assertThat(arr1.toList()).containsExactly(0,1,2);
+    }
+    @Test
+    void add_last_then_first() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addLast(1);
+        arr1.addLast(2);
+        arr1.addFirst(0);
+        assertThat(arr1.toList()).containsExactly(0,1,2);
+    }
+    @Test
+    void add_first_then_last() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addFirst(1);
+        arr1.addFirst(0);
+        arr1.addLast(2);
+        assertThat(arr1.toList()).containsExactly(0,1,2);
+    }
+    @Test
+    void add_first_to_resize() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        for (int i = 9; i >= 0; i--) {
+            arr1.addFirst(i);
+        }
+        assertThat(arr1.toList()).containsExactly(0,1,2,3,4,5,6,7,8,9);
+    }
+    @Test
+    void add_last_to_resize() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        for (int i = 0; i < 10; i++) {
+            arr1.addLast(i);
+        }
+        assertThat(arr1.toList()).containsExactly(0,1,2,3,4,5,6,7,8,9);
+    }
+    @Test
+    void get_normal() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addLast(0);
+        arr1.addLast(1);
+        arr1.addLast(2);
+        assertThat(arr1.get(1)).isEqualTo(1);
+    }
+    @Test
+    void get_out_of_range() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addLast(0);
+        arr1.addLast(1);
+        arr1.addLast(2);
+        assertThat(arr1.get(43)).isNull();
+    }
+    @Test
+    void is_empty_true() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        assertThat(arr1.isEmpty()).isTrue();
+    }
+    @Test
+    void is_empty_false() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addLast(0);
+        arr1.addLast(1);
+        arr1.addLast(2);
+        assertThat(arr1.isEmpty()).isFalse();
+    }
+    @Test
+    void size() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        arr1.addLast(0);
+        arr1.addLast(1);
+        arr1.addLast(2);
+        assertThat(arr1.size()).isEqualTo(3);
+    }
+    @Test
+    void add_last_remove_last() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        for (int i = 0; i < 20; i++) {
+            arr1.addLast(i);
+        }
+        for (int i = 0; i < 17; i++) {
+            arr1.removeLast();
+        }
+        assertThat(arr1.toList()).containsExactly(0,1,2);
+    }
+    @Test
+    void add_first_remove_last() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        for (int i = 0; i < 20; i++) {
+            arr1.addFirst(i);
+        }
+        for (int i = 0; i < 17; i++) {
+            arr1.removeLast();
+        }
+        assertThat(arr1.toList()).containsExactly(19,18,17);
+    }
+    @Test
+    void add_first_remove_first() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        for (int i = 0; i < 20; i++) {
+            arr1.addFirst(i);
+        }
+        for (int i = 0; i < 17; i++) {
+            arr1.removeFirst();
+        }
+        assertThat(arr1.toList()).containsExactly(0,1,2);
+    }
+    @Test
+    void add_last_remove_first() {
+        Deque<Integer> arr1 = new ArrayDeque<>();
+        for (int i = 0; i < 20; i++) {
+            arr1.addLast(i);
+        }
+        for (int i = 0; i < 17; i++) {
+            arr1.removeFirst();
+        }
+        assertThat(arr1.toList()).containsExactly(19,18,17);
+    }
 }
