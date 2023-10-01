32d4c29db09d4ad5eee2e73fb885b804feabecd3
Harshit Kolisetty <harshitk@berkeley.edu>
2023-09-30 17:32:06 -0700

    Im stupid and stubborn (lab06)

--- lab06/src/UnionFind.java
+++ lab06/src/UnionFind.java
@@ -81,19 +81,27 @@ public class UnionFind {
        root to V2's root. Union-ing a item with itself or items that are
        already connected should not change the structure. */
     public void union(int v1, int v2) {
-        int rootv1 = find(v1);
-        int rootv2 = find(v2);
-        int larger = rootv2;
-        int smaller = rootv1;
+        helper(find(v1), find(v2));
+
+    }
+    public void helper(int v1, int v2) {
+        int larger = v2;
+        int smaller = v1;
         if (sizeOf(v1) > sizeOf(v2)) {
-            larger = rootv1;
-            smaller = rootv2;
+            larger = v1;
+            smaller = v2;
         }
-        if (larger != smaller) {
+        if (size[smaller] != 1) {
+            for (int i = 0; i < data.length; i++) {
+                if (data[i] == smaller) {
+                    helper(i, larger);
+                }
+            }
+        }
+        if (smaller != larger) {
             data[smaller] = larger;
             size[larger] = size[larger] + size[smaller];
         }
-
     }
 
     /**
