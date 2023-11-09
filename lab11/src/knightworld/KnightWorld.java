52e4ccb20a29dd00b52ca782ee7b7e8de0eef309
Harshit Kolisetty <harshitk@berkeley.edu>
2023-11-08 22:15:41 -0800

    Finished lab11

--- lab11/src/knightworld/KnightWorld.java
+++ lab11/src/knightworld/KnightWorld.java
@@ -2,6 +2,11 @@ package knightworld;
 
 import tileengine.TERenderer;
 import tileengine.TETile;
+import tileengine.Tileset;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Draws a world consisting of knight-move holes.
@@ -15,6 +20,34 @@ public class KnightWorld {
         // TODO: Fill in this constructor and class, adding helper methods and/or classes as necessary to draw the
         //  specified pattern of the given hole size for a window of size width x height. If you're stuck on how to
         //  begin, look at the provided demo code!
+        tiles = new TETile[height][width];
+        Random RANDOM = new Random();
+        int rand = RANDOM.nextInt(5);
+        int rand1 = RANDOM.nextInt(4);
+        int rand2 = RANDOM.nextInt(4);
+        Map<Integer, Integer> cool = new HashMap<>() {{
+            put(0, 2);
+            put(1, 4);
+            put(2, 1);
+            put(3, 3);
+            put(4, 0);
+        }};
+        for (int i = 0; i < Math.ceilDiv(height, holeSize); i++) {
+            for (int j = 0; j < Math.ceilDiv(width, holeSize); j++) {
+                for (int k = 0; k < holeSize; k++) {
+                    for (int l = 0; l < holeSize; l++) {
+                        if (i * holeSize + k >= height || j * holeSize + l >= width) {
+                            continue;
+                        }
+                        if ((i - cool.get(j % 5) - rand) % 5 == 0) {
+                            tiles[i * holeSize + k][j * holeSize + l] = Tileset.NOTHING;
+                        } else {
+                            tiles[i * holeSize + k][j * holeSize + l] = Tileset.LOCKED_DOOR;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     /** Returns the tiles associated with this KnightWorld. */
@@ -24,14 +57,14 @@ public class KnightWorld {
 
     public static void main(String[] args) {
         // Change these parameters as necessary
-        int width = 50;
-        int height = 30;
-        int holeSize = 2;
+        int width = 60;
+        int height = 40;
+        int holeSize = 4;
 
         KnightWorld knightWorld = new KnightWorld(width, height, holeSize);
 
         TERenderer ter = new TERenderer();
-        ter.initialize(width, height);
+        ter.initialize(height, width);
         ter.renderFrame(knightWorld.getTiles());
 
     }
--- lab11/src/project3prep.md
+++ lab11/src/project3prep.md
@@ -2,22 +2,22 @@
 
 **What distinguishes a hallway from a room? How are they similar?**
 
-Answer:
+Answer: A hallway has a constant width of 1 tile with a random length. Whereas a room can have a random width and length. Both have random length.
 
 **Can you think of an analogy between the process of 
 drawing a knight world and randomly generating a world 
 using rooms and hallways?**
 
-Answer:
+Answer: Just how I used conditionals to determine which areas should be empty to create the knight world, I can use similar spatial relations to determine how far apart certain rooms and hallways should be.
 
 **If you were to start working on world generation, what kind of method would you think of writing first? 
 Think back to the lab and the process used to eventually 
 get to the full knight world.**
 
-Answer:
+Answer: I would write the constructor method that initializes the random seed for the world generation. I would also create a method that determines which tile type to use based its spatial distance to other items.
 
 **This question is open-ended. Write some classes 
 and methods that might be useful for Project 3. Think 
 about what helper methods and classes you used for the lab!**
 
-Answer:
+Answer: World(), createHallway(), Room(), createArea(), getItemType(tile), getDistancetoPlayer() 
